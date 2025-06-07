27cda0bad8a47933561dcdeb38f8d982084c7b0f
vladuka6 <vladuka6@gmail.com>
2025-06-07 22:24:16 +0300

    оновлено для вебхук

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-BOT_TOKEN = "7988184310:AAFEU86dvc5_dOJdLrrB9QDTcm_O1bTWGLU"  # Замените на реальный токен
-OPENWEATHER_API_KEY = "e0904bca4dfb0fcd415d4a0bc2509201"  # Ключ с openweathermap.org (бесплатно 1000 юзов каждый день)
-OPENWEATHER_BASE_URL = "https://api.openweathermap.org/data/2.5"
-
+import os
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7988184310:AAFEU86dvc5_dOJdLrrB9QDTcm_O1bTWGLU")
+WEBHOOK_URL = os.getenv("WEBHOOK_URL", "https://weather-bot-vqyf.onrender.com/webhook")
+OPENWEATHER_API_KEY = os.getenv("OPENWEATHER_API_KEY", "e0904bca4dfb0fcd415d4a0bc2509201")
 DB_NAME = "weather_bot.db"
